b271881272eb966656f9e77d89524e437e4597de
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2026-01-12 22:56:33 +1000

    1. Local dev env setup w XAMPP

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "student_db";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+echo "Database connected successfully!";
+?>
